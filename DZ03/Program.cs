0d7a14b48f74e6582591838fdbc235b3d4a2aa1d
Semyon <tonkih.sem@mail.ru>
2022-09-16 21:32:07 +0500

    Добавил домашнее задание: Задача 6

---
+++ DZ03/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая на вход принимает число и 
+выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber % 2 == 0)
+{
+    Console.WriteLine("Ответ: Да.");
+}
+else 
+{
+    Console.WriteLine("Ответ: Нет.");
+}
